bd1f003d73e4ab63e601f4a93ed0715f4fcefd12
mortezamollaie <mm.gov.1381@gmail.com>
2025-04-05 23:09:56 +0330

    implementing login service

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,13 +2,25 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\ApiResponse\Facades\ApiResponse;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Services\Auth\AuthService;
 
 class LoginController extends Controller
 {
-    public function __invoke()
+    public function __construct(public AuthService  $authService)
     {
+    }
 
+    public function __invoke(LoginRequest $request)
+    {
+        $result = $this->authService->login($request->validated());
+        if(!$result->ok){
+            return ApiResponse::withMessage('Something went wrong. try again later...')->withStatus(500)->build()->response();
+        } else if ($result->data == 'failed'){
+            return ApiResponse::withMessage('Email or password was wrong')->withStatus(400)->build()->response();
+        }
+        return ApiResponse::withMessage('User logged in successfully.')->withStatus(200)->withData($result->data)->build()->response();
     }
 }
--- app/Services/Auth/AuthService.php
+++ app/Services/Auth/AuthService.php
@@ -16,8 +16,18 @@ class AuthService
         });
     }
 
-    public function login()
+    public function login(array $data)
     {
+        return app(ServiceWrapper::class)(function() use ($data){
+            if(!auth()->attempt($data))
+                return 'failed';
 
+            $user = auth()->user();
+            $token = $user->createToken('API TOKEN')->plainTextToken;
+            return [
+                'user' => $user,
+                'token' => $token
+            ];
+        });
     }
 }
